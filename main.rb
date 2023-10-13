c03640e389b57bf2a1970d7bb878b78d8f2d4fe9
O16215 <O16215@lab.aca>
2023-10-13 11:22:30 -0500

    prueba 1

--- main.rb
+++ main.rb
@@ -1,5 +1,33 @@
+if __FILE__ != $0 then exit end
+
 Dir["clases/persona/*.rb"].each { |clasePer| require_relative clasePer }
 Dir["clases/media/*.rb"].each { |claseMe| require_relative claseMe }
+
+puts "***************************************"
+puts "**********               **************"
+puts "********** biblioteca ul **************"
+puts "**********               **************"
+puts "***************************************"
+
+while true do
+    puts "1. Ver libros"
+    puts "2. Ver usuarios"
+    puts "3. Salir"
+    opcion = gets.to_i
+    if opcion == 3 then 
+        break
+    else 
+        puts "ok"
+        case opcion
+        when 1
+            puts "hacer 1"
+        when 2
+            puts "hacer 2"
+        else
+            puts "no hacer nada"
+        end
+    end
+end
 
 # TEST
 x = Alumno.new()
