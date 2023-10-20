2430b8b58a02cfbe7dae109f1772e7c9e1a7e8a7
O16215 <O16215@lab.aca>
2023-10-20 12:08:51 -0500

    test

--- main.rb
+++ main.rb
@@ -7,36 +7,68 @@ Dir["clases/media/*.rb"].each { |claseMe| require_relative claseMe }
 
 puts "***************************************"
 puts "**********               **************"
-puts "********** biblioteca ul **************"
+puts "*********  biblioteca ul  *************"
 puts "**********               **************"
 puts "***************************************"
 
-# TEST
-x = Alumno.new(1, "a", "b", "c")
-y = Profesor.new(2, "a", "b", "c")
-# z = Usuario.new() <= Agarra excepcion
-
+# TEST LIBROS
+libros = []
 w = Libro.new()
 w.datos("el","pepe","123")
 # w1 = Material.new() <= Agarra excepcion
+libros << w
 
+# TEST ALUMNOS
+alumnos = []
+x = Alumno.new(1, "a", "b", "c")
+x1 = Alumno.new(2, "d", "e", "f")
+x2 = Alumno.new(3, "g", "h", "i")
+alumnos << x
+alumnos << x1
+alumnos << x2
+
+profesores = []
+y = Profesor.new(10, "a", "b", "c")
+# z = Usuario.new() <= Agarra excepcion
+profesores << y
+
+def printLibro(libro)
+    "- Libro #{libro.titulo} del autor #{libro.autor} perteneciente al area #{libro.area}"
+end
+
+def printAlumno(alumno)
+    "- Alumno #{alumno.apellidoPaterno} #{alumno.apellidoMaterno}, #{alumno.nombre} de codigo #{alumno.codAlumno}"
+end
+
+def printProfesor(profesor)
+    "- Profesor #{profesor.apellidoPaterno} #{profesor.apellidoMaterno}, #{profesor.nombre} de codigo #{profesor.codProfesor}"
+end
+
+# Flujo terminal
 while true do
     puts "1. Ver libros"
     puts "2. Ver usuarios"
     puts "3. Salir"
     opcion = gets.to_i
+    # Salir
     if opcion == 3 then 
         break
-    else 
-        puts "ok"
+    else
+        # Opcion 1 y 2
         case opcion
         when 1
-            puts w.titulo
+            for i in libros do
+                puts printLibro(i)
+            end
         when 2
-            puts "- Alumno #{x.apellidoPaterno} #{x.apellidoMaterno} #{x.nombre} de codigo #{x.codAlumno}"
-            # puts y.maxLibro
+            for i in alumnos do
+                puts printAlumno(i)
+            end
+            for j in profesores do
+                puts printProfesor(j)
+            end
         else
-            puts "no hacer nada"
+            puts "Opción inválida."
         end
     end
 end
