c6c4cae5579d8dd28c61d5fb8679d68ef99cc479
O16215 <O16215@lab.aca>
2023-10-20 11:39:01 -0500

    clases

---
+++ clases/media/revista_act-congreso.rb
@@ -0,0 +1,6 @@
+require_relative "revista.rb"
+
+class ActaCongreso < Revista
+    def initialize()
+    end
+end
---
+++ clases/media/revista_journal.rb
@@ -0,0 +1,6 @@
+require_relative "revista.rb"
+
+class Journal < Revista
+    def initialize()
+    end
+end
